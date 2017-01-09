9b98a96241e4e26eb9eb9ac28e7be455cc49edf1
Sarai Díaz <dgarciasarai@gmail.com>
2017-01-09 13:00:38 +0100

    ImageGallery screenlet iOS

--- app/pages/gallery/gallery.component.ts
+++ app/pages/gallery/gallery.component.ts
@@ -3,6 +3,10 @@ import { ScreensContext } from "../../shared/context/screens-context.service";
 import app = require("application");
 
 declare var com: any;
+declare var NSObject: any;
+declare var CGRectMake: any;
+declare var ImageGalleryScreenlet: any;
+declare var ImageGalleryScreenletDelegate: any;
 
 @Component({
     selector: "gallery",
@@ -16,34 +20,65 @@ export class ImageGalleryComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.renderGallery()
+    }
+
+    renderGallery() {
         console.log('trying to render gallery');
 
-        let context = app.android.context;
+        let gallery;
 
-        let gallery = new com.liferay.mobile.screens.imagegallery.ImageGalleryScreenlet(context);
+        if (app.android) {
+            let context = app.android.context;
 
-        gallery.setRepositoryId(this.screensContext.GROUP_ID);
-        gallery.setFolderId(this.screensContext.IMAGE_GALLERY_FOLDER_ID);
-        gallery.setAutoLoad(true);
-        gallery.setGroupId(this.screensContext.GROUP_ID);
-        gallery.setFirstPageSize(10);
-        gallery.setPageSize(10);
-        let remoteOnly = com.liferay.mobile.screens.cache.CachePolicy.REMOTE_ONLY;
-        gallery.setCachePolicy(remoteOnly);
-        let picasso = com.liferay.mobile.screens.context.PicassoScreens;
-        picasso.setCachePolicy(remoteOnly);
-        let localeUS = java.util.Locale.US;
-        gallery.setLocale(localeUS);
-        let layout = context.getResources().getIdentifier("gallery_custom", "layout", context.getPackageName());
+            gallery = new com.liferay.mobile.screens.imagegallery.ImageGalleryScreenlet(context);
 
-        gallery.render(layout);
+            gallery.setRepositoryId(this.screensContext.GROUP_ID);
+            gallery.setFolderId(this.screensContext.IMAGE_GALLERY_FOLDER_ID);
+            gallery.setAutoLoad(true);
+            gallery.setGroupId(this.screensContext.GROUP_ID);
+            gallery.setFirstPageSize(10);
+            gallery.setPageSize(10);
+            let remoteOnly = com.liferay.mobile.screens.cache.CachePolicy.REMOTE_ONLY;
+            gallery.setCachePolicy(remoteOnly);
+            let picasso = com.liferay.mobile.screens.context.PicassoScreens;
+            picasso.setCachePolicy(remoteOnly);
+            let localeUS = java.util.Locale.US;
+            gallery.setLocale(localeUS);
+            let layout = context.getResources().getIdentifier("gallery_custom", "layout", context.getPackageName());
 
-        console.log(gallery);
+            gallery.render(layout);
 
-        let containerId = context.getResources().getIdentifier("content" , "id", "android");
+            console.log(gallery);
+
+            this.attach(gallery, context);
+
+        } else {
+            console.log("iOS!!!!");
+
+            let superview = app.ios.rootController.view;
+
+            gallery = new ImageGalleryScreenlet(CGRectMake(0, 20, 375, 667));
+            gallery.repositoryId = this.screensContext.GROUP_ID;
+            gallery.folderId = this.screensContext.IMAGE_GALLERY_FOLDER_ID;
+            gallery.autoload = true;
+            gallery.groupId = this.screensContext.GROUP_ID;
+            gallery.pageSize = 10;
+            gallery.firstPageSize = 10;
+
+            console.log("login screenlet ios: " + gallery);
+            console.log("theme name ios: " + gallery.themeName);
+
+            superview.addSubview(gallery);
+
+        }
+    }
+
+    attach(view, context) {
+        let containerId = context.getResources().getIdentifier("content", "id", "android");
         let activity = app.android.foregroundActivity;
         let container = activity.findViewById(containerId);
         let matchParent = android.widget.LinearLayout.LayoutParams.MATCH_PARENT;
-        container.addView(gallery, matchParent, matchParent);
+        container.addView(view, matchParent, matchParent);
     }
 }
