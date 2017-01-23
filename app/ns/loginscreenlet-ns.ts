b1e641d762efbf750671c893e5c25230176265b5
Sarai Díaz <dgarciasarai@gmail.com>
2017-01-23 17:17:35 +0100

    Custom LoginScreenlet component to be injected in another one

--- app/ns/loginscreenlet-ns.ts
+++ app/ns/loginscreenlet-ns.ts
@@ -1,68 +1,70 @@
 import {Component} from "@angular/core";
 import {ScreensContext} from "../shared/context/screens-context.service";
-import {Router} from "@angular/router";
-import app = require("application");
+import app = require('application');
 
 declare let com: any;
+declare let CGRectMake: any;
+declare let LoginScreenlet: any;
 
 @Component({
     selector: "loginscreenlet",
     providers: [ScreensContext],
     template: ``
 })
-export class LoginScreenlet {
+export class LoginScreenletNs {
 
     public login: any;
-    public loginListener: any;
-    public context = app.android.context;
+    public view: any;
 
-    constructor(private router: Router, private screensContext: ScreensContext) {
+    constructor(private screensContext: ScreensContext) {
+        this.createScreenlet("default");
+    }
 
+    createScreenlet(theme) {
         if (app.android) {
-            let self = this;
-            this.login = new com.liferay.mobile.screens.auth.login.LoginScreenlet(this.context);
-
+            this.login = new com.liferay.mobile.screens.auth.login.LoginScreenlet(app.android.context);
             let credentials = com.liferay.mobile.screens.context.storage.CredentialsStorageBuilder.StorageType.NONE;
             this.login.setCredentialsStorage(credentials);
 
+            this.setLayout("login_" + theme);
+            this.setAuthMethod(com.liferay.mobile.screens.auth.BasicAuthMethod.EMAIL);
+            this.setAuthType(com.liferay.mobile.screens.context.AuthenticationType.BASIC);
         } else {
-            console.log("loginscreenlet ns");
+            this.login = new LoginScreenlet(CGRectMake(20, 40, 335, 617), theme);
+            app.ios.rootController.view.addSubview(this.login);
         }
     }
-    
-    initialize(loginLayout, authMethod) {
-        console.log("login on init...: " + loginLayout);
 
-        let layout = this.context.getResources().getIdentifier(loginLayout, "layout", this.context.getPackageName());
-        let layoutInflater = android.view.LayoutInflater;
-        let view: any = layoutInflater.from(this.context).inflate(layout, null);
-        
-        view.setBasicAuthMethod(authMethod);
-
-        let basic = com.liferay.mobile.screens.context.AuthenticationType.BASIC;
-        view.setAuthenticationType(basic);
-
-        console.log("login rendered");
-
-        this.attach(view);
-    }
-
-    attach(view) {
-        let containerId = this.context.getResources().getIdentifier("content", "id", "android");
+    attach() {
+        let containerId = app.android.context.getResources().getIdentifier("content", "id", "android");
         let activity = app.android.foregroundActivity;
         let container = activity.findViewById(containerId);
         let matchParent = android.widget.LinearLayout.LayoutParams.MATCH_PARENT;
-        container.addView(view, matchParent, matchParent);
+        container.addView(this.view, matchParent, matchParent);
     }
 
     getScreenlet() {
         return this.login;
     }
 
+    setLayout(loginLayout) {
+        let layout = app.android.context.getResources().getIdentifier(loginLayout, "layout", app.android.context.getPackageName());
+        let layoutInflater = android.view.LayoutInflater;
+        this.view = layoutInflater.from(app.android.context).inflate(layout, null);
+    }
+
+    setAuthType(authType) {
+        this.view.setAuthenticationType(authType);
+    }
+
+    setAuthMethod(authMethod) {
+        this.view.setBasicAuthMethod(authMethod);
+    }
+
     setListener(listener) {
         if (this.login != null) {
             console.log(listener);
-            this.loginListener = this.getScreenlet().setListener(listener);
+            this.getScreenlet().setListener(listener);
         }
     }
 }
